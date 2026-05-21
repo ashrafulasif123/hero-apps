46c30b4b6143fb02a493525a73c5d33c26ef3cb1
ashrafulasif123 <cseasifiiuc9@gmail.com>
2026-05-21 18:07:38 +0600

    pending banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,7 +1,11 @@
 const Banner = () => {
     return (
-        <div>
-
+        <div className="">
+            <div className="text-center">
+                <div className="max-w-160 text-center border">
+                    <h1 className="text-7xl">We Build <span>Productive</span> Apps</h1>
+                </div>
+            </div>
         </div>
     );
 };

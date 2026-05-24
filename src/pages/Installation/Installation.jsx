5429b223d108ea78e7574802f88398242862d838
ashrafulasif123 <cseasifiiuc9@gmail.com>
2026-05-24 15:44:29 +0600

    ins

--- src/pages/Installation/Installation.jsx
+++ src/pages/Installation/Installation.jsx
@@ -3,32 +3,32 @@ import { useLoaderData } from "react-router";
 import { getInstalledAppIdsFromLS } from "../../utilities/utilities";
 import Title from "../../components/Title/Title";
 import InstalledApps from "../InstalledApps/InstalledApps";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 
 const Installation = () => {
     const apps = useLoaderData()
 
     const previousInstalledId = () => getInstalledAppIdsFromLS().map(i => parseInt(i))
-    const [installedId, setInstalledId] = useState(() => previousInstalledId())
-    const [installedApps, setInstalledApps] = useState(() => apps.filter(app => ))
-
+    const [installedIds, setInstalledIds] = useState(() => previousInstalledId())
+    const [installedApps, setInstalledApps] = useState(() => apps.filter(app => installedIds.includes(app.id)))
+    console.log(installedApps)
 
     return (
         <div className="bg-base-200 py-15">
             <Title title="Your Installed Apps" subTitle="Explore All Trending Apps on the Market developed by us"></Title>
             <div className="py-12 max-w-400 mx-auto">
-                <div className="flex justify-between items-center py-5">
-                    <h2 className="font-bold text-2xl">({installedAppIds.length}) Installed Apps </h2>
+                {/* <div className="flex justify-between items-center py-5">
+                    <h2 className="font-bold text-2xl">({installedApps.length}) Installed Apps </h2>
                     <select defaultValue="Medium" className="select select-md">
                         <option disabled={true}>Sort By Download</option>
                         <option onClick={() => handleSortByDownload("default")}>Default</option>
                         <option onClick={() => handleSortByDownload("high-to-low")}>High to Low</option>
                         <option onClick={() => handleSortByDownload("low-to-high")}>Low to High</option>
                     </select>
-                </div>
+                </div> */}
                 <div className="flex flex-col gap-y-4">
                     {
-                        installedApps.map(installedApp => <InstalledApps key={installedApp.id} installedApp={installedApp} handleUninstall={handleUninstall}></InstalledApps>)
+                        installedApps.map(installedApp => <InstalledApps key={installedApp.id} installedApp={installedApp} ></InstalledApps>)
                     }
                 </div>
             </div>
